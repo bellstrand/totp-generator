3dfb9e3c1cd35090ac762745651b8b5c8ef8145c
Magnus Bellstrand <bellstrand@gmail.com>
2016-06-07 21:09:40 +0200

    jasmine tests added

---
+++ test/index.spec.js
@@ -0,0 +1,13 @@
+let totp = require('../index');
+
+describe('totp generation', () => {
+	it('should generate token with date now = 1971', () => {
+		global.Date.now = () => { return 0; };
+		expect(totp('JBSWY3DPEHPK3PXP')).toEqual(282760);
+	});
+
+	it('should generate token with date now = 2016', () => {
+		global.Date.now = () => { return 1465324707000; };
+		expect(totp('JBSWY3DPEHPK3PXP')).toEqual(341128);
+	});
+});
