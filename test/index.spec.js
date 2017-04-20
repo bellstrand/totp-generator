fdc17811352ae74f2e23ec44d6747c39622c6ebd
Magnus Bellstrand <bellstrand@gmail.com>
2017-04-20 20:33:45 +0200

    feat(parsing): removed parseInt of token, getToken now returns string, fixes issued with leading zeroes being stripped

--- test/index.spec.js
+++ test/index.spec.js
@@ -5,11 +5,16 @@ let totp = require('../index');
 describe('totp generation', () => {
 	it('should generate token with date now = 1971', () => {
 		global.Date.now = () => { return 0; };
-		expect(totp('JBSWY3DPEHPK3PXP')).toEqual(282760);
+		expect(totp('JBSWY3DPEHPK3PXP')).toEqual('282760');
 	});
 
 	it('should generate token with date now = 2016', () => {
 		global.Date.now = () => { return 1465324707000; };
-		expect(totp('JBSWY3DPEHPK3PXP')).toEqual(341128);
+		expect(totp('JBSWY3DPEHPK3PXP')).toEqual('341128');
+	});
+
+	it('should generate token with a leading zero', () => {
+		global.Date.now = () => { return 1365324707000; };
+		expect(totp('JBSWY3DPEHPK3PXP')).toEqual('089029');
 	});
 });
